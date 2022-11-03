c7bcc511e6ec09633bb47d33ff580e3c550c384e
青山研人 <106291245+KentoAoyama@users.noreply.github.com>
2022-11-03 16:00:54 +0900

    [add]Pauseを実行するPauseControllerを追加

--- Assets/Scripts/GameState/GameStateManager.cs
+++ Assets/Scripts/GameState/GameStateManager.cs
@@ -1,20 +1,29 @@
+using UnityEngine;
+using System;
+
 public class GameStateManager
 {
+    //クラスをSingletonにする
+    private static GameStateManager _instance = new();
+    public static GameStateManager Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                Debug.LogError($"Error! Please correct!");
+            }
+            return _instance;
+        }
+    }
+    private GameStateManager() { }
+
     private static InGameState _gameState;
 
     /// <summary>
-    /// 外部からStateを参照して確認する用のプロパティ
+    /// 外部からStateを参照、変更する用のプロパティ
     /// </summary>
-    public InGameState GameState => _gameState;
-
-    /// <summary>
-    /// 外部からStateを変更する用のメソッド
-    /// </summary>
-    /// <param name="gameState">変更するState</param>
-    public static void ChangeState(InGameState gameState)
-    {
-        _gameState = gameState;
-    }
+    public InGameState GameState { get => _gameState; set => _gameState = value; }
 
     public enum InGameState
     {
@@ -37,11 +46,6 @@ public class GameStateManager
         /// イベントムービーが流れている状態
         /// </summary>
         Movie,
-
-        /// <summary>
-        /// ゲームがポーズ中であることを表す
-        /// </summary>
-        Pause,
 
         /// <summary>
         /// 次元の変更中であることを表す
---
+++ Assets/Scripts/Pause/PauseController.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class PauseController : MonoBehaviour
+{
+    /// <summary>
+    /// ゲームがポーズ中であるかを表す変数
+    /// </summary>
+    private bool _isPause = false;
+
+    void Update()
+    {
+        ///ボタンが押されたらポーズを実行
+        if (Input.GetKeyDown(KeyCode.P)) //ここの押されるボタンは後で変更
+        {
+            
+            if (!_isPause)
+            {
+                //ポーズ中でないならPauseの処理を実行
+                PauseManager.Instance.OnPause?.Invoke();
+            }
+            else
+            {
+                ////ポーズ中ならResumeの処理を実行
+                PauseManager.Instance.OnResume?.Invoke();
+            }
+            
+            _isPause = !_isPause;
+        }
+    }
+}
