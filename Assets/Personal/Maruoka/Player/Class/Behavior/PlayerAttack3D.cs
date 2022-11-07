ac64bf02a02b54aed50eeb9406b484be8121b66f
HarumiMaruoka <106292983+HarumiMaruoka@users.noreply.github.com>
2022-11-07 22:20:12 +0900

    [update] Playerを更新

--- Assets/Personal/Maruoka/Player/Class/Behavior/PlayerAttack3D.cs
+++ Assets/Personal/Maruoka/Player/Class/Behavior/PlayerAttack3D.cs
@@ -4,8 +4,35 @@ using UnityEngine;
 [System.Serializable]
 public class PlayerAttack3D : PlayerAttack
 {
+    public PlayerAttack3D(
+        PlayerStateController state,
+        string fireButtonName, Transform transform,
+        Vector3 firePosOffset, Vector3 fireSize,
+        LayerMask targetLayer) :
+        base(state, fireButtonName, transform,
+            firePosOffset, fireSize, targetLayer)
+    {
+
+    }
+
     public override void OnFire()
     {
-        // throw new NotImplementedException();
+        Debug.Log("3DUŒ‚‚ğÀs‚µ‚½");
+
+        // UŒ‚‘ÎÛ‚ğæ“¾‚·‚é
+        var pos = GetFirePos();
+
+        var colliders = Physics.OverlapBox(
+            pos, _fireSize, Quaternion.identity, _targetLayer);
+
+        // UŒ‚ˆ—‚ğÀs‚·‚é
+        foreach (var e in colliders)
+        {
+            Debug.Log($"\"{e.name}\"‚ÉUŒ‚‚µ‚½");
+            // if(e.TryGetComponent(out EnemyController enemy))
+            // {
+            //     enemy.Damage();
+            // }
+        }
     }
 }
