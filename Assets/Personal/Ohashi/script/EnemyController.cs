eeff603f3b05a463a85b221a1edc166ad2dd4dc3
RK <riku.kuu0421@gmail.com>
2022-11-08 15:24:37 +0900

    PlayerSearchメソッドの修正とエネミーの移動処理を追加

--- Assets/Personal/Ohashi/script/EnemyController.cs
+++ Assets/Personal/Ohashi/script/EnemyController.cs
@@ -1,16 +1,17 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
+    private EnemyMove _enemyMove;
     void Start()
     {
-        
+        _enemyMove = GetComponent<EnemyMove>();
     }
 
     void Update()
     {
-        
+        _enemyMove.PlayerSearch();
+        _enemyMove.Move();
+        Debug.Log(_enemyMove.PlayerSearch());
     }
 }
--- Assets/Personal/Ohashi/script/EnemyMove.cs
+++ Assets/Personal/Ohashi/script/EnemyMove.cs
@@ -2,13 +2,37 @@ using UnityEngine;
 
 public class EnemyMove : MonoBehaviour
 {
-    [SerializeField, Tooltip("プレイヤーの座標")] private Transform _player;
+    [SerializeField, Tooltip("プレイヤーの座標")] private Transform _playerTransform;
+    [SerializeField, Tooltip("移動するかどうかの距離")] private float _moveDistance = 5.0f;
+    [SerializeField, Tooltip("移動の速さ")] private float _moveSpeed;
 
+    private Rigidbody _rb;
+
+    private void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+    }
     /// <summary>
     /// playerとenemyのX軸の二点間の差で距離を測る
     /// </summary>
-    private void PlayerSearch()
+    public bool PlayerSearch()
     {
-        float player = _player.position.x - transform.position.x;
+        float playerDistans = _playerTransform.position.x - transform.position.x;
+        if(_moveDistance > playerDistans && -_moveDistance < playerDistans)
+        {
+            return true;
+        }
+        return false;
+    }
+    /// <summary>
+    /// エネミーの移動処理
+    /// </summary>
+    public void Move()
+    {
+        if(!PlayerSearch())
+        {
+            Vector3 target = (_playerTransform.position - transform.position).normalized;
+            _rb.velocity = new Vector3(target.x * _moveSpeed, 0, 0);
+        }
     }
 }
