d79df2e5bc33c1b42dfa03b64546960ba6ec7f29
HarumiMaruoka <106292983+HarumiMaruoka@users.noreply.github.com>
2022-11-11 15:59:13 +0900

    [update] 今日の成果　設計の変更完了

--- Assets/Personal/Maruoka/Old/Player/Class/BehaviorBases/PlayerMove.cs
+++
@@ -1,9 +0,0 @@
-using System;
-using UnityEngine;
-
-[System.Serializable]
-public abstract class PlayerMove
-{
-    protected abstract void Init(Component rb);
-    public abstract void Move();
-}
--- Assets/Personal/Maruoka/Player/Class/Damage3D.cs
+++ Assets/Personal/Maruoka/Player/Class/Damage3D.cs
@@ -1,10 +1,13 @@
 using System;
 using UnityEngine;
 using UniRx;
+using System.Threading.Tasks;
+using System.Threading;
 
 [System.Serializable]
 public class Damage3D : PlayerDamage
 {
+
     private Rigidbody _rb = default;
 
     public void Init(Rigidbody rb)
@@ -12,18 +15,30 @@ public class Damage3D : PlayerDamage
         _rb = rb;
 
         // UniRx Test Code
-        PlayerStatusManager.Instance.Life.Skip(1).Subscribe(
-            x => Debug.Log($"体力の変化を検知しました。現在の体力は{x}です。"));
+        // PlayerStatusManager.Instance.Life.Skip(1).Subscribe(
+        //     x => Debug.Log($"体力の変化を検知しました。現在の体力は{x}です。"));
     }
 
-    public override void OnDamage(int value)
+    public override async void OnDamage(int value,
+        Vector3 knockBackDir, float knockBackPower,
+        int knockBackTime)
     {
-        if (_isGodMode)
+        if (!_isGodMode)
         {
+            if (_isTest)
+            {
+                value = _testDamageValue;
+                knockBackDir = _testKnockBackDir;
+                knockBackPower = _testKnockBackPower;
+                knockBackTime = _testKnockBackTime;
+            }            
             // ノックバック処理
-            // _rb.AddForce(dir * power, ForceMode.Impulse);
+            _rb.AddForce(knockBackDir.normalized * knockBackPower, ForceMode.Impulse);
             // 体力を減らす
             PlayerStatusManager.Instance.Damage(value);
+
+            // ノックバック中、RigidBody Velocity の更新を止める
+            await KnockBackStart(knockBackTime);
         }
     }
 }
