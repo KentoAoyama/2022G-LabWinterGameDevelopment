cc21c3f5d8e54cdd979b3eb322dc7b2b9b1a94dc
RK <riku.kuu0421@gmail.com>
2022-11-11 18:22:36 +0900

    [add]アセンブリ追加とEnemyBulletController追加

--- Assets/Personal/Ohashi/script/Enemy2DComponent/EnemyMove2D.cs
+++ Assets/Personal/Ohashi/script/Enemy2DComponent/EnemyMove2D.cs
@@ -7,13 +7,9 @@ public class EnemyMove2D : EnemyMove
 {
     private Rigidbody2D _rb2D;
 
-    private void Start()
-    {
-        _rb2D = GetComponent<Rigidbody2D>();
-    }
     protected override void RbMove()
     {
-        Vector3 target = (_playerTransform.position - transform.position).normalized;
+        Vector3 target = (_playerTransform.position - _transform.position).normalized;
         _rb2D.velocity = new Vector3(target.x * _moveSpeed, 0, 0);
     }
 
