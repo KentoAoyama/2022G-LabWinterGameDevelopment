0cca1dec00d2750d23d33b90373bb7e110cbfb3e
青山研人 <106291245+KentoAoyama@users.noreply.github.com>
2022-11-14 19:42:57 +0900

    [update]IGimmickEventに追記

--- Assets/Scripts/Interface/IGimmickEvent.cs
+++ Assets/Scripts/Interface/IGimmickEvent.cs
@@ -1,5 +1,12 @@
+using UnityEngine;
+
 public interface IGimmickEvent
 {
+    /// <summary>
+    /// プレイヤーのアニメーションを再生するかを判定する用のプロパティ
+    /// </summary>
+    public bool IsPlayAnimation { get; }
+
     /// <summary>
     /// IGameEventによって実装される、ギミックが作動した時に実行されるメソッド
     /// </summary>
