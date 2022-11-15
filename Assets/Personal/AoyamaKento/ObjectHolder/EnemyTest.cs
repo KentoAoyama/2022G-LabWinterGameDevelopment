69595ffd0ca33bea568e41318658e50605ce4fe8
青山研人 <106291245+KentoAoyama@users.noreply.github.com>
2022-11-15 11:15:44 +0900

    [update]Dimention進捗

--- Assets/Personal/AoyamaKento/ObjectHolder/EnemyTest.cs
+++ Assets/Personal/AoyamaKento/ObjectHolder/EnemyTest.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 public class EnemyTest : RetainedEnemyBehavior
 {
     [SerializeField] private int _hp = 3;
-    [Range(1, 2), SerializeField] private int _id = 1;
+    [Range(0, 1), SerializeField] private int _id = 1;
 
     protected override int Id => _id;
     protected override int Health { get => _hp; set => _hp = value; }
