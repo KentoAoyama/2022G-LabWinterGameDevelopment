f4eb94f51985115faa5b8634143e4114c783b39a
青山研人 <106291245+KentoAoyama@users.noreply.github.com>
2022-11-27 09:54:38 +0900

    [fix]バグを削除（仮）

--- Assets/Personal/Watanabe/GimmickController.cs
+++ Assets/Personal/Watanabe/GimmickController.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 /// 通知が入ったらGimmickが持っているイベントを実行する
 /// イベントが実行された時セーブを実行する(DataSave.csで実行)?
 /// </summary>
-public abstract class GimmickController : MonoBehaviour, IGimmickEvent
+public abstract class GimmickController : MonoBehaviour//, IGimmickEvent
 {
     [Header("体温関連")]
     [Tooltip("テスト用の体温")]
