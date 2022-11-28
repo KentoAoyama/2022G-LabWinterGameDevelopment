85ea24148b678637ae3d24f32d65a5fe1bb2d873
UME_ROUTE49 <umemomosakura0725@icloud.com>
2022-11-28 13:27:47 +0900

    GUI HelpMenuを完成させる

---
+++ Assets/Personal/Toyama/BonFire2D.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BonFire2D : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        //‰·“x‚ğ‰ñ•œ‚·‚éˆ—
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        //‰ñ•œ‚ğ~‚ß‚éˆ—
+    }
+}
