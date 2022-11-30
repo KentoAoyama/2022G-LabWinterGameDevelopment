12fa692222b1eccf4bd85265ada4d96c39caa7a3
RK <riku.kuu0421@gmail.com>
2022-11-30 11:10:48 +0900

    [add]エネミーIDを追加

---
+++ Assets/Personal/Ohashi/script/EnemyEnum/EnemyId.cs
@@ -0,0 +1,6 @@
+[System.Serializable]
+public enum EnemyId
+{
+    Short,
+    Long,
+}
--- Assets/Personal/Ohashi/script/IEnemyAddDamage.cs
+++
@@ -1,7 +0,0 @@
-public interface IEnemyAddDamage
-{
-    /// <summary>
-    /// エネミーがダメージを受けた時の処理
-    /// </summary>
-    public void EnemyAddDamage(int damage);
-}
