b0f63bbd8b912ebf58a16d9a358a56625c7e669b
RK <riku.kuu0421@gmail.com>
2022-11-30 11:17:46 +0900

    [add]エネミーバレットを3D,2D追加

---
+++ Assets/Personal/Ohashi/script/EnemyBase/EnemyAttackBase.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using System.Threading.Tasks;
+
+public abstract class EnemyAttackBase
+{
+    private const int MilliSecond = 1000;
+    protected bool _isAttack = false;
+
+    /// <summary>
+    /// エネミーの攻撃処理
+    /// </summary>
+    public abstract void EnemyAttack();
+    /// <summary>
+    /// エネミーの攻撃のインターバル
+    /// </summary>
+    protected async Task EnemyAttackInterval(int attackInterval)
+    {
+        _isAttack = true;
+        Debug.Log("attack");
+        //指定したミリ秒後に実行する
+        await Task.Delay(MilliSecond * attackInterval);
+        _isAttack = false;
+    }
+}
