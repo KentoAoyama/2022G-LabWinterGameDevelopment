8c75225dd6a9fba21d31c126745e80f7be52bf43
SHUNSHUN-0031 <saruwatarishun15@gmail.com>
2022-12-18 23:31:11 +0900

    titleシーン

--- Assets/Personal/Saruwatari/ScenChenji.cs
+++ Assets/Personal/Saruwatari/ScenChenji.cs
@@ -3,14 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using DG.Tweening;
 
 public class ScenChenji : MonoBehaviour
 {
-    [SerializeField] Image titleLogo;
+    [SerializeField] Slider titleLogo;
     [SerializeField] Button strat;
     [SerializeField] Button opushon;
     [SerializeField] Button help;
     [SerializeField] Button credit;
+    [SerializeField] Image creditgamenn;
+
 
     // Start is called before the first frame update
     void Start()
@@ -23,16 +26,38 @@ public class ScenChenji : MonoBehaviour
     {
         if (Input.GetButton("Fire1"))
         {
+
+            // DOTween.To() を使って連続的に変化させる
+            DOTween.To(() => titleLogo.value, // 連続的に変化させる対象の値
+                x => titleLogo.value = x, // 変化させた値 x をどう処理するかを書く
+                (float)0 / (float)100, // x をどの値まで変化させるか指示する
+                2f);   // 何秒かけて変化させるか指示する
+            StartCoroutine(Strat());
+        }
+    }
+
+    private IEnumerator Strat()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(2.1f);
             titleLogo.gameObject.SetActive(false);
             strat.gameObject.SetActive(true);
             opushon.gameObject.SetActive(true);
             help.gameObject.SetActive(true);
             credit.gameObject.SetActive(true);
+
         }
+
     }
 
     public void LoadSceme(string sceneName)
     {
         SceneManager.LoadScene(sceneName);
+    }
+
+    public void Credit()
+    {
+        creditgamenn.gameObject.SetActive(true);
     }
 }
