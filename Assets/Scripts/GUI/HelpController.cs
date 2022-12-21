44dadbcc502a8c987accc900d98ecb4e6df935f7
青山研人 <106291245+KentoAoyama@users.noreply.github.com>
2022-12-21 17:13:22 +0900

    [update]マージ作業

--- Assets/Scripts/GUI/HelpController.cs
+++ Assets/Scripts/GUI/HelpController.cs
@@ -12,7 +12,8 @@ public class HelpController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        helpMenu.SetActive(false);
+        helpCursor.SetActive(false);
     }
 
     // Update is called once per frame
